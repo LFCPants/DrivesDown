2136cfabf12b412c64e6f77596022e5b218049ee
LFCPants <77917715+LFCPants@users.noreply.github.com>
2021-03-07 11:23:33 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1353008.torrent
+wget https://bangumi.moe/download/torrent/601bb41e32f14c00073f1683/[VCB-Studio&philosophy-raws]%20Owari%20no%20Seraph%20_%20%E7%BB%88%E7%BB%93%E7%9A%84%E7%82%BD%E5%A4%A9%E4%BD%BF%20_%20%E7%B5%82%E3%82%8F%E3%82%8A%E3%81%AE%E3%82%BB%E3%83%A9%E3%83%95%2010-bit%201080p%20HEVC%20BDRip%20[Reseed%20Fin].torrent
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=20; i<=1064; i=i+20, j=j+20))
 #do

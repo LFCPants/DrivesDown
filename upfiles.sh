a78201cff4303cb1fd06da90db0ee9519794e237
LFCPants <77917715+LFCPants@users.noreply.github.com>
2021-03-07 10:44:10 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://bangumi.moe/torrent/6042ff9032f14c00074aedd6
+wget https://share.acgnx.se/down.php?date=1615003537&hash=070dd4c38de078518324e38511b228bb6f823193
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=20; i<=1064; i=i+20, j=j+20))
 #do

0929ada20b6b23bcd858ebd4afd2b6b929aaabea
LFCPants <77917715+LFCPants@users.noreply.github.com>
2021-03-11 12:46:47 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -6,5 +6,5 @@ aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ animation:/文档/
 #echo "$i-$j 上传完成"
-#done0
+#done
 

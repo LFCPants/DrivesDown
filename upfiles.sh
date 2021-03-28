89df863965a6f6f069589d5781c6e9ec9543a324
LFCPants <77917715+LFCPants@users.noreply.github.com>
2021-03-28 19:15:34 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://bangumi.moe/download/torrent/5cf0aaad475bb7b273365f36/[SweetSub&VCB-Studio]%20Mirai%20no%20Mirai%20_%20%E6%9C%AA%E6%9D%A5%E7%9A%84%E6%9C%AA%E6%9D%A5%20_%20%E6%9C%AA%E6%9D%A5%E3%81%AE%E3%83%9F%E3%83%A9%E3%82%A4%2010-bit%201080p%20HEVC%20BDRip%20[Fin].torrent
+wget https://nyaa.si/download/1142186.torrent
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=15; i<=1064; i=i+15, j=j+15))
 #do

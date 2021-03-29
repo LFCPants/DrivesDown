39eec13c127911ade163049cf5ad1f0fb7a1a89c
LFCPants <77917715+LFCPants@users.noreply.github.com>
2021-03-30 00:58:11 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1114706.torrent
+wget https://bangumi.moe/download/torrent/5c4d6c2660a958730e1238e3/[VCB-Studio]%20Batman_%20The%20Dark%20Knight%20Returns%20_%20%E8%9D%99%E8%9D%A0%E4%BE%A0%EF%BC%9A%E9%BB%91%E6%9A%97%E9%AA%91%E5%A3%AB%E5%BD%92%E6%9D%A5%2010-bit%201080p%20HEVC%20BDRip%20[Movie%20Fin].torrent
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=15; i<=1064; i=i+15, j=j+15))
 #do

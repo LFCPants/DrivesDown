f9c103e78d9baa8820e09a37d6bff09bfcf16c56
LFCPants <77917715+LFCPants@users.noreply.github.com>
2021-03-30 16:09:21 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1083027.torrent
+wget https://bangumi.moe/download/torrent/5bb47459de4560f455f61bb1/[VCB-Studio]%20Batman_%20The%20Killing%20Joke%20_%20%E8%9D%99%E8%9D%A0%E4%BE%A0%EF%BC%9A%E8%87%B4%E5%91%BD%E7%8E%A9%E7%AC%91%2010-bit%201080p%20HEVC%20BDRip%20[Movie%20Fin].torrent
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=15; i<=1064; i=i+15, j=j+15))
 #do

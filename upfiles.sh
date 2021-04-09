61cce5d298e38444e96916366bab9697b7058914
LFCPants <77917715+LFCPants@users.noreply.github.com>
2021-04-09 10:23:40 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget https://nyaa.si/download/992580.torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-for ((i=2715, j=2717; i<=2999; i=i+2, j=j+2))
+for ((i=2721, j=2723; i<=2999; i=i+2, j=j+2))
 do
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ onedrive:/"我的文件 - animation_0 的文件"/番/

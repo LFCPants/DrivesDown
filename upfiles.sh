c35da8e908181a7258e62375abc0320b25c2f6d8
LFCPants <77917715+LFCPants@users.noreply.github.com>
2021-04-30 19:22:00 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1378254.torrent
+wget https://nyaa.si/download/960014.torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 for ((i=1, j=10; i<=600; i=i+10, j=j+10))
 do

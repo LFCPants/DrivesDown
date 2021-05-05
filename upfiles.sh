9233d0ca9c4c365bfb1da7388325ffa278f7ae72
LFCPants <77917715+LFCPants@users.noreply.github.com>
2021-05-05 17:48:21 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://nyaa.si/download/936272.torrent
+wget https://bangumi.moe/download/torrent/58a2f3dd45a01953431c687f/[VCB-Studio]%20High%20School%20Fleet_%E9%AB%98%E6%A0%A1%E8%88%B0%E9%98%9F%2010-bit%201080p%20HEVC%20BDRip%20[Fin].torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-for ((i=1, j=10; i<=600; i=i+10, j=j+10))
+for ((i=1, j=10; i<=700; i=i+10, j=j+10))
 do
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ onedrive:/"我的文件 - animation_1 的文件"/番/

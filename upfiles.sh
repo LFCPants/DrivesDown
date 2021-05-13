39787a63acc01ad1228ed42026206154a04ab185
LFCPants <77917715+LFCPants@users.noreply.github.com>
2021-05-13 19:21:54 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://bangumi.moe/download/torrent/5927fc3f1a0b230822448ee2/[VCB-Studio]%20Regalia%20The%20Three%20Sacred%20Stars%20_%20%E9%9B%B7%E5%8A%A0%E5%88%A9%E4%BA%9A%20%E4%B8%89%E5%9C%A3%E6%98%9F%20_%20%E3%83%AC%E3%82%AC%E3%83%AA%E3%82%A2%2010-bit%201080p%20HEVC%20BDRip%20[Fin].torrent
+wget https://bangumi.moe/download/torrent/592fab836cf8d8694c4efd8c/[VCB-Studio]%20Gochuumon%20wa%20Usagi%20Desuka__%20Rabbit%20House%20Tea%20Party%202016%20_%20%E8%AF%B7%E9%97%AE%E6%82%A8%E4%BB%8A%E5%A4%A9%E8%A6%81%E6%9D%A5%E7%82%B9%E5%85%94%E5%AD%90%E5%90%97%EF%BC%9F%EF%BC%9F%20_%20%E3%81%94%E6%B3%A8%E6%96%87%E3%81%AF%E3%81%86%E3%81%95%E3%81%8E%E3%81%A7%E3%81%99%E3%81%8B__%2010bit%201080p%20BDRip.torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-for ((i=31, j=40; i<=910; i=i+10, j=j+10))
+for ((i=1, j=10; i<=910; i=i+10, j=j+10))
 do
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ onedrive:/"我的文件 - animation_1 的文件"/番/

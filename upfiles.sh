094c7ee12564fa6ce0a11da8c8f287ea0ae93dc2
LFCPants <77917715+LFCPants@users.noreply.github.com>
2021-05-17 21:35:36 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://link.jscdn.cn/sharepoint/aHR0cHM6Ly9zaHRoZXktbXkuc2hhcmVwb2ludC5jb20vOnU6L2cvcGVyc29uYWwvaHJmX3NodGhleV9vbm1pY3Jvc29mdF9jb20vRWZOZEhBTEFDbGxPaTdHQURzbWhnVzhCcm4zTFN3R3A3cHFPUENxcHRjQzJVdz9lPThMdmZscg.torrent
+wget http://storage.live.com/items/014GNKTQ7TLUOAFQAKLFHIXMMAB3E2DALP:/fczlm.torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-for ((i=1, j=2; i<=50; i=i+2, j=j+2))
+for ((i=2, j=2; i<=50; i=i+2, j=j+2))
 do
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ onedrive:/fczlm/
